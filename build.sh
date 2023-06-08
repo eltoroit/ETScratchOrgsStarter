9dbe72154f7f24431f13b0e8ea3d4248dad642bb
Andrés Pérez (eltoroit) <andresperezf@yahoo.com>
2023-06-08 10:32:23 -0400

    Update build script to enforce sorce-tracked orgs

--- build.sh
+++ build.sh
@@ -1,6 +1,8 @@
 #!/bin/zsh
-echo "sf project deploy start --ignore-conflicts --verbose --json"
-sf project deploy start --ignore-conflicts --verbose --json 
+exe() { echo "\$ $@" ; "$@" ; }
+# Do not use sf project deploy start because it does not detect if this is a sorce-tracked org and you could accidentally deploy to production!
+# exe sf project deploy start --target-org soPackageTester --ignore-conflicts --verbose --json 
+exe sfdx force source push --forceoverwrite --json
 if [ $? -eq 0 ] 
 then 
     print "\u001b[42m\u001b[1;30m*** *** SUCCESS *** ***\u001b[0m"
